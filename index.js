0c03ca296c4ded5cd442ddf4df67db72d9480533
DannnLim <Dannylim-@hotmail.com>
2022-06-03 17:34:23 +1000

    code-along

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = "Danny";
+
+// console.log(currentUser.length)
+
+const welcomeMessage = "Welcome to Flatbook,  " + currentUser + '!';
+
+// console.log(welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome,  ${currentUser.slice(0, 1)}!`;
+
+// console.log(shortGreeting)
