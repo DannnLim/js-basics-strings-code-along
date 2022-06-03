14111fd2aaf2511308ad04fb448edb619d5d47b1
DannnLim <Dannylim-@hotmail.com>
2022-06-03 17:14:30 +1000

    commit tree 4275694745661d6d021409908473de1e77d8cc82

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = "Danny";
+
+const welcomeMessage = "Welcome to Flatbook,  " + currentUser + '!';
+
+// console.log(welcomeMessage)
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// console.log(excitedWelcomeMessage)
+
+const shortGreeting = `Welcome,  ${currentUser.slice(0, 1)}!`;
